8059a26fed3d768223c242278b3eeafd8583b0c3
Rcasco <rogercasco1989@gmail.com>
2018-01-14 16:55:44 -0600

    add frequency service

---
+++ my-web-api/Controllers/FrequencyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using ExpenseItApi.Models;
+using ExpenseItApi.DataAccess;
+using AutoMapper;
+
+namespace ExpenseItApi.Controllers
+{
+    [Route("api/Frequency")]
+	[EnableCors("AllowAllHeaders")]
+	public class FrequencyController : Controller
+	{
+		private readonly FrequencyService _service;
+
+		public FrequencyController(ExpenseItContext context, IMapper mapper)
+		{
+            _service = new FrequencyService(context, mapper);
+		}
+
+		[HttpGet]
+		public IEnumerable<Frequency> GetAll()
+		{
+            return _service.GetAllFrequencies();
+		}
+
+		[HttpGet("{id}", Name = "GetFrequency")]
+		public IActionResult GetById(int id)
+		{
+            var item = _service.GetFrequencyById(id);
+			if (item == null)
+			{
+				return NotFound();
+			}
+			return new ObjectResult(item);
+		}
+	}
+}
---
+++ my-web-api/DataAccess/FrequencyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ExpenseItApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseItApi.DataAccess
+{
+    public class FrequencyService
+    {
+        private readonly ExpenseItContext _context;
+        private readonly IMapper _mapper;
+
+        public FrequencyService(ExpenseItContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public IEnumerable<Frequency> GetAllFrequencies() 
+        {
+            return _context.Frequency
+                                   .AsNoTracking()
+                                   .ToList();
+        }
+
+        public Frequency GetFrequencyById(int id)
+        {
+            return _context.Frequency.FirstOrDefault(t => t.id == id);
+        }
+    }
+}
