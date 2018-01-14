5255bf9d1159f48e3805f572b6d8d4b51a199bba
Rcasco <rogercasco1989@gmail.com>
2018-01-14 16:25:37 -0600

    config

--- my-web-api/Program.cs
+++ my-web-api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System.IO;
 using TodoApi;
 
 namespace my_web_api
--- my-web-api/Startup.cs
+++ my-web-api/Startup.cs
@@ -38,6 +38,8 @@ namespace TodoApi
 		{
             app.UseCors("AllowAllHeaders");
 			app.UseMvc();
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
 		}
 	}
 }
